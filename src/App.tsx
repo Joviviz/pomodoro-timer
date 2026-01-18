fd978645c34ac8c4c2bb5bd89d658131b1168bc3
Joviviz <101208535+Joviviz@users.noreply.github.com>
2026-01-18 11:32:02 -0300

    Create container component practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 
 export function App() {
-
   return (
     <>
-      <Heading>Ola mundo 1</Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum sunt
-        consectetur a iusto sapiente voluptate nisi fugiat! Pariatur, iure
-        molestiae molestias sit repellat veritatis, inventore non harum unde
-        incidunt amet.
-      </p>
+      <Container>
+        <Heading>Logo</Heading>
+      </Container>
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
     </>
   );
 }
