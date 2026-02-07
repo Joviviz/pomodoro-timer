ff2cdf40b805d1806681441f08a2d5b03c8a5bde
Joviviz <101208535+Joviviz@users.noreply.github.com>
2026-02-07 18:12:03 -0300

    Add css Styles for main page, create Input Container

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,50 @@ import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 
 export function App() {
   return (
     <>
       <Container>
-        <Logo/>
+        <Logo />
       </Container>
-      
+
       <Container>
-        <Menu/>
+        <Menu />
       </Container>
-      
+
       <Container>
-        <CountDown/>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput
+              id='meuInput'
+              type='number'
+              labelText='task'
+              placeholder='Digite algo' 
+              defaultValue='Valor preenchido'
+            />
+          </div>
+
+          <div className='formRow'>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. dolorum?
+            </p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
